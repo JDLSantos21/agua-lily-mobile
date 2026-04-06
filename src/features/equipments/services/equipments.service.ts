f4019b095c51c54dee0c9b680fd815d7897203fa
JDLSantos21 <jdlsantos21@hotmail.com>
2026-04-05 22:57:48 -0400

    refactor: remove debug log for location retrieval in saveEquipmentLocation function

--- src/features/equipments/services/equipments.service.ts
+++ src/features/equipments/services/equipments.service.ts
@@ -3,7 +3,6 @@ import { updateEquipmentLocation } from "./equipments.api";
 
 export const saveEquipmentLocation = async (id: number) => {
   const { latitude, longitude } = await getCurrentLocation();
-  console.log("Ubicación obtenida:", latitude, longitude);
 
   const res = await updateEquipmentLocation(id, latitude, longitude);
 
